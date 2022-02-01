7cc4f8e7ba63b3f2ce64ad56ea263c4ddce63007
FBub <fbub@dfau.de>
2022-02-01 12:37:38 +0100

    Add responsive feature to website and glide arrows

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,9 +1,9 @@
-const menuLink = document.querySelectorAll('menu__link');
+const menuLinks = document.querySelectorAll('a.menu__link');
 const checkboxSwitch = document.querySelector('#switch');
 
 
-menuItems.forEach(function (menuItem) {
-    menuItem.onclick = function () {
+menuLinks.forEach( function (menuLink) {
+    menuLink.onclick = function () {
         checkboxSwitch.checked = false
     };
 });
